6c9d963e946ca1437f0c9e259709a1eb37edf1c3
GiangBeo <giangbeo@MacOSs-MacBook-Pro.local>
2019-02-12 02:11:35 +0700

    Update config

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,12 +2,11 @@ class UserController < ApplicationController
   before_action :set_user
 
   def index
-    json_response(@attributes.itself)
+    json_response(@user.itself)
   end
 
   def set_user
     @user = User.find(params[:user_id])
-    @attributes = @user
   end
 
   def json_response(object, status = :ok)
