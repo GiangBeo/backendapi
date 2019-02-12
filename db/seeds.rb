46568072bff74536417865e15c9470bb986d92ec
giangnt2 <giangnt2@sendo.vn>
2019-02-12 13:55:05 +0700

    Update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([{name: 'Alice'}])
+User.create([{name: 'Alice B'}])
+Account.create([{id: 1, name: 'A銀行', balance: 20000, user_id: 1}])
+Account.create([{id: 3, name: 'C信用金庫', balance: 120000, user_id: 1}])
+Account.create([{id: 5, name: 'E銀行', balance: 5000, user_id: 1}])
+Account.create([{id: 2, name: 'Bカード', balance: 200, user_id: 2}])
